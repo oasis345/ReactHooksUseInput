de63e40179b499ad74d655a05735256743591fcd
oasis345 <cogudrnr9@naver.com>
2020-03-25 01:50:41 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+export const useInput = (initialValue ,validator) => {
+  const [value , setValue] = useState(initialValue)
+  const onChange = event =>{
+    const {
+      target:   {value}
+  } = event
+  let willUpdate = true
+  if(typeof validator === "function"){
+    willUpdate = validator(value)
+  }
+  if(willUpdate){
+    setValue(value)
+  }
+}
+
+  return { value ,onChange};
+}
+
+
+const App=()=> {
+  // const maxLen = value => value.length <= 10
+  const maxLen = value => !value.includes("@")
+  const name = useInput("Mr.", maxLen)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello</h1>
+      {/* 3점 표기법으로 name안의 내용을 풀어준다 꿀팁! 아래가 같은소스인데 내용이 확 줄게된다. */}
+      <input placeholder="name" {...name}/>  
+      {/* <input placeholder="name" value={name.value} onChange={name.onChange}/> */}
     </div>
   );
 }
